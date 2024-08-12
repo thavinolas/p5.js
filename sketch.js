35367e0280428fadf6b03f69f22134797139b4be
Thaviny Campos. <thavinycampos0409@gmail.com>
2024-08-12 15:17:36 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background("white")
+}
+
+function draw() {
+  stroke("blue");
+  fill("pink");
+  
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
